9cb31910e76d9b3229d579aa3c0618a819490523
Cat Chen <catchen@catchen.me>
2023-03-26 21:12:53 -0700

    Expanded all functions to support package.json and lock file content as input

--- src/getDependencyTree.ts
+++ src/getDependencyTree.ts
@@ -1,28 +1,64 @@
 import { existsSync } from 'node:fs';
 import { resolve } from 'node:path';
-import { buildDepTreeFromFiles } from 'snyk-nodejs-lockfile-parser';
+import {
+  LockfileType,
+  buildDepTree,
+  buildDepTreeFromFiles,
+} from 'snyk-nodejs-lockfile-parser';
 import type { PkgTree } from 'snyk-nodejs-lockfile-parser';
+
+type PackagePath = string;
+type NpmPackageFiles = {
+  packageJsonContent: string;
+  packageLockJsonContent: string;
+};
+type YarnPackageFiles = {
+  packageJsonContent: string;
+  YarnLockContent: string;
+};
+export type NodePackage = PackagePath | NpmPackageFiles | YarnPackageFiles;
 
 /**
  * Get dependency tree of a given package directory.
  * @param root A package's root directory that containers the package.json file with either package-lock.json or yarn.lock file.
  * @returns A tree that represents all the packages in the dependency tree.
  */
-export async function getDependencyTree(root: string): Promise<PkgTree> {
-  const absoluteRootPath = resolve(process.cwd(), root);
-  const absolutePackageJsonPath = resolve(absoluteRootPath, 'package.json');
-  const absolutePackageLockJsonPath = resolve(
-    absoluteRootPath,
-    'package-lock.json',
-  );
-  const absoluteYarnLockPath = resolve(absoluteRootPath, 'yarn.lock');
-  const absoluteLockFilePath = existsSync(absolutePackageLockJsonPath)
-    ? absolutePackageLockJsonPath
-    : absoluteYarnLockPath;
-  return buildDepTreeFromFiles(
-    absoluteRootPath,
-    absolutePackageJsonPath,
-    absoluteLockFilePath,
-    true,
-  );
+export async function getDependencyTree(
+  nodePackage: NodePackage,
+): Promise<PkgTree> {
+  if (typeof nodePackage === 'string') {
+    const packagePath = nodePackage;
+    const absoluteRootPath = resolve(process.cwd(), packagePath);
+    const absolutePackageJsonPath = resolve(absoluteRootPath, 'package.json');
+    const absolutePackageLockJsonPath = resolve(
+      absoluteRootPath,
+      'package-lock.json',
+    );
+    const absoluteYarnLockPath = resolve(absoluteRootPath, 'yarn.lock');
+    const absoluteLockFilePath = existsSync(absolutePackageLockJsonPath)
+      ? absolutePackageLockJsonPath
+      : absoluteYarnLockPath;
+    return buildDepTreeFromFiles(
+      absoluteRootPath,
+      absolutePackageJsonPath,
+      absoluteLockFilePath,
+      true,
+    );
+  } else if ('packageLockJsonContent' in nodePackage) {
+    return buildDepTree(
+      nodePackage.packageJsonContent,
+      nodePackage.packageLockJsonContent,
+      true,
+      LockfileType.npm,
+    );
+  } else {
+    return buildDepTree(
+      nodePackage.packageJsonContent,
+      nodePackage.YarnLockContent,
+      true,
+      nodePackage.YarnLockContent.includes('__metadata')
+        ? LockfileType.yarn2
+        : LockfileType.yarn,
+    );
+  }
 }
--- src/isExclusiveDevDependency.ts
+++ src/isExclusiveDevDependency.ts
@@ -1,4 +1,5 @@
 import { findDependency } from './findDependency';
+import type { NodePackage } from './getDependencyTree';
 
 /**
  * Determines if a given package name is used exclusively as a dev dependency in a given package directory.
@@ -7,9 +8,9 @@ import { findDependency } from './findDependency';
  * @returns An indicator of whether the given package name is used exclusively as a dev dependency.
  */
 export async function isExclusiveDevDependency(
-  root: string,
+  nodePackage: NodePackage,
   name: string,
 ): Promise<boolean> {
-  const dependencies = await findDependency(root, name);
+  const dependencies = await findDependency(nodePackage, name);
   return dependencies.every((dependency) => dependency.labels?.scope === 'dev');
 }
